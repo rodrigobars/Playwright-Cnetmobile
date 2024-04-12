2b9700d89478122f96476eba3d509d9616dabbd7
Rodrigo B <rodrigobs.uff@gmail.com>
2024-04-11 22:23:19 -0300

    up

--- groupGetter.js
+++ groupGetter.js
@@ -15,8 +15,43 @@ const getGrupoPropostas = async () => {
             setTimeout(() => {
                 button.click();
             }, delay);
-            delay += 1500
+            delay += 3000
         }
     }, 100);
 
 }
+
+
+
+//elementos por índice:
+// buttonNodes[0] - Botão principal de expansão
+// buttonNodes[1] - Chat
+// buttonNodes[2] - Proposta
+// buttonNodes[buttonNodes.length - 1]; - Anexos
+
+// const dataViewContent = document.getElementsByClassName('p-dataview-content');
+// const dataViewContentListNodes = dataViewContent[0].childNodes;
+// const divNodes = Array.prototype.filter.call(dataViewContentListNodes, function(node) {
+//     return node.nodeType === Node.ELEMENT_NODE && node.tagName === 'DIV';
+// });
+
+// for (const companyNode of divNodes) {
+    
+//     setTimeout(() => {
+//         const buttonNodes = companyNode.getElementsByTagName('BUTTON');
+        
+//         buttonNodes[0].click();
+//         buttonNodes[2].click();
+
+//         let delay = 1000;
+
+//         const qtdItens = buttonNodes.length - 4
+    
+//         for (let i = 3; i < (2*qtdItens)+3; i+=2) {
+//             setTimeout(() => {
+//                 buttonNodes[i].click();
+//             }, delay);
+//             delay += 3000
+//         }
+//     }, 400)
+// }
